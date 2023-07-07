a9a1d3cbe87e1dc9934b7f79b8afa97c7debbf7b
Johan Löfgren <johan.lofgren@iver.se>
2023-07-07 15:14:21 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import styled from 'styled-components'
 
-import About from 'components/About'
-import Portfolio from 'components/Portfolio'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Dashboard from './pages/Dashboard'
+import LogIn from './pages/LogIn'
 
 const Main = styled.div`
   display: flex;
@@ -24,6 +25,7 @@ export const App = () => {
           <Route exact path="/" element={<Portfolio />} />
           <Route exact path="/About" element={<About />} />
           <Route exact path="/Dashboard" element={<Dashboard />} />
+          <Route exact path="/Login" element={<LogIn />} />
         </Routes>
         <Footer />
       </BrowserRouter>
